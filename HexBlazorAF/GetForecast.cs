f24aa0af719c88bfb61e4d83d810d2c5465d3ae3
Gerald Moore <gerald.moore@orau.org>
2021-03-16 16:06:59 -0400

    set temps in F, calculate C

--- HexBlazorAF/GetForecast.cs
+++ HexBlazorAF/GetForecast.cs
@@ -17,16 +17,16 @@ namespace HexBlazorAF
 
             HexBlazorLib.WeatherForecast[] forecast = new HexBlazorLib.WeatherForecast[]
             {
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,09), TemperatureC = 18, Summary = "Freezing"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,10), TemperatureC = 27, Summary = "Freezing"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,11), TemperatureC = 36, Summary = "Bracing"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,12), TemperatureC = 45, Summary = "Cool"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,13), TemperatureC = 56, Summary = "Cool"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,14), TemperatureC = 64, Summary = "Perfect"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,15), TemperatureC = 73, Summary = "Perfect"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,16), TemperatureC = 82, Summary = "Warm"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,17), TemperatureC = 91, Summary = "Hot"},
-                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,18), TemperatureC = 99, Summary = "Hot"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,09), TemperatureF = 18, Summary = "Freezing"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,10), TemperatureF = 27, Summary = "Freezing"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,11), TemperatureF = 36, Summary = "Bracing"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,12), TemperatureF = 45, Summary = "Cool"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,13), TemperatureF = 56, Summary = "Cool"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,14), TemperatureF = 64, Summary = "Perfect"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,15), TemperatureF = 73, Summary = "Perfect"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,16), TemperatureF = 82, Summary = "Warm"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,17), TemperatureF = 91, Summary = "Hot"},
+                new HexBlazorLib.WeatherForecast() { Date = new System.DateTime(2018,05,18), TemperatureF = 99, Summary = "Hot"},
             };
 
             return new OkObjectResult(forecast);
--- HexBlazorLib/WeatherForecast.cs
+++ HexBlazorLib/WeatherForecast.cs
@@ -6,10 +6,17 @@ namespace HexBlazorLib
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
         public string Summary { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public double TemperatureF { get; set; }
+
+        public double TemperatureC
+        {
+            get
+            {
+                return Math.Round((TemperatureF - 32d) * (5d/9d),1);
+            }
+        }
+
     }
 }
